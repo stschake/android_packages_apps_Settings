d0e715c58c851c2271ca66579d717c8d480b0ce3
Igor Kalkov <ikalkov@users.noreply.github.com>
2016-06-21 16:45:01 +0200

    Use latest RTAndroid API

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -64,7 +64,7 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import rtandroid.RealTimeProxy;
+import rtandroid.realtime.RealTimeProxy;
 
 public class DeviceInfoSettings extends SettingsPreferenceFragment implements Indexable {
 
